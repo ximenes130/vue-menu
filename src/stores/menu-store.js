ad96ed7744a9eaf589f8459f34670c73c02b306c
Luiz Ximenes <ximenes130@gmail.com>
2022-07-12 00:02:20 -0300

    Add menu data to localStorage

--- src/stores/menu-store.js
+++ src/stores/menu-store.js
@@ -3,14 +3,15 @@ import menuItems from 'src/data/MenuItems';
 
 export const useMenuStore = defineStore('menu', {
   state: () => ({
-    menuItems: menuItems,
+    menuItems: JSON.parse(localStorage.getItem('menuItems')) || menuItems,
   }),
   getters: {
     favorites: (state) => state.menuItems.filter(item => item.favorite),
   },
   actions: {
-    favoriteItem({ state, item }) {
-      item.favorite = !item.favorite;
+    favoriteItem({ item }) {
+      item.favorite = !item.favorite
+      localStorage.setItem('menuItems', JSON.stringify(this.menuItems))
     }
   },
 });
